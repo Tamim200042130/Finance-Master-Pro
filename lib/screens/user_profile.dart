7fa4cf352ee11203f57dfc8031eefa5ed2831010
Md.Siamul Mubin Tamim <95132931+Tamim200042130@users.noreply.github.com>
2024-03-14 19:51:30 +0600

    user-profile-fix

--- lib/screens/user_profile.dart
+++ lib/screens/user_profile.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 
+import '../services/auth_service.dart';
 import 'edit_profile.dart';
+import 'login_screen.dart';
 
 class UserProfilePage extends StatefulWidget {
   @override
@@ -13,71 +15,197 @@ class UserProfilePage extends StatefulWidget {
 
 class _UserProfilePageState extends State<UserProfilePage> {
   late User _user;
-  late DocumentSnapshot _userDoc;
+  late Future<DocumentSnapshot> _userDataFuture;
+  var authService = AuthService();
+  var isLogoutLoading = false;
+  late String _currentTime;
 
   @override
   void initState() {
     super.initState();
     _user = FirebaseAuth.instance.currentUser!;
-    _fetchUserData();
+    _userDataFuture = _fetchUserData();
+    _currentTime = '';
+    _updateTimeEverySecond();
   }
 
-  Future<void> _fetchUserData() async {
+  String _getCurrentTime() {
+    return DateFormat('dd MMMM yyyy hh:mm:ss a').format(DateTime.now());
+  }
+
+  void _updateTimeEverySecond() {
+    Future.delayed(Duration(seconds: 1)).then((_) {
+      setState(() {
+        _currentTime = _getCurrentTime();
+      });
+      _updateTimeEverySecond();
+    });
+  }
+
+  Future<DocumentSnapshot> _fetchUserData() async {
     final userDocSnapshot = await FirebaseFirestore.instance
         .collection('users')
         .doc(_user.uid)
         .get();
-    setState(() {
-      _userDoc = userDocSnapshot;
-    });
+    return userDocSnapshot;
+  }
+
+  _dialogLogout(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Color(0xFF252634),
+          title: const Text(
+            'Are you sure?',
+            style: TextStyle(color: Colors.white),
+          ),
+          content: const Text(
+            'Do you want to logout?',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: 20,
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                  color: Colors.yellowAccent,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                setState(() {
+                  isLogoutLoading = true;
+                });
+                authService.logout();
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => Login()),
+                );
+                setState(() {
+                  isLogoutLoading = false;
+                });
+              },
+              child: const Text(
+                'Logout',
+                style: TextStyle(
+                  color: Colors.redAccent,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    DateTime? date;
-    String formattedDate = '';
-
-    date = DateTime.fromMillisecondsSinceEpoch(_userDoc['updatedAt']);
-    formattedDate = DateFormat('dd MMMM yyyy hh:mm a').format(date);
-
     return Scaffold(
       backgroundColor: const Color(0xFF252634),
       appBar: AppBar(
         backgroundColor: const Color(0xFF252634),
         title: Center(
-            child: Text('User Profile',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                ))),
+          child: Text(
+            'User Profile',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+          ),
+        ),
+        // leading: IconButton(
+        //   icon: Icon(FontAwesomeIcons.circleArrowLeft,
+        //       color: Colors.yellowAccent[700]),
+        //   onPressed: () {
+        //     Navigator.pop(context);
+        //   },
+        // ),
         actions: [
-          IconButton(
-            icon: Icon(FontAwesomeIcons.userPen,
-                size: 25, color: Colors.yellowAccent[700]),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditProfilePage(userDoc: _userDoc),
-                ),
-              );
+          FutureBuilder(
+            future: _userDataFuture,
+            builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return IconButton(
+                  icon: Icon(Icons.error),
+                  onPressed: () {
+                    // Handle error
+                  },
+                );
+              } else {
+                return IconButton(
+                  icon: Icon(FontAwesomeIcons.userPen,
+                      size: 25, color: Colors.yellowAccent[700]),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            EditProfilePage(userDoc: snapshot.data!),
+                      ),
+                    );
+                  },
+                );
+              }
             },
           ),
         ],
-        leading: IconButton(
-          icon: Icon(FontAwesomeIcons.circleArrowLeft,
-              size: 30, color: Colors.yellowAccent[700]),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
       ),
-      body: _userDoc != null
-          ? Padding(
+      body: FutureBuilder(
+        future: _userDataFuture,
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else {
+            final userDoc = snapshot.data!;
+            DateTime? date;
+            String formattedDate = '';
+
+            date = DateTime.fromMillisecondsSinceEpoch(userDoc['updatedAt']);
+            formattedDate = DateFormat('dd MMMM yyyy hh:mm a').format(date);
+
+            return Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Container(
+                    width: double.infinity,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent.shade400,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            '$_currentTime',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
                   Container(
                     width: double.infinity,
                     height: 60,
@@ -90,16 +218,53 @@ class _UserProfilePageState extends State<UserProfilePage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Center(
-                            child: Expanded(
+                          Text(
+                            'Username: ${userDoc['username']}',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.yellowAccent[700],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Phone Number: ${userDoc['phoneNumber']}',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 2),
+                          SizedBox(
+                            width: 380, // Adjust the width as needed
+                            child: Center(
                               child: Text(
-                                'Username: ${_userDoc['username']}',
+                                'Email: ${_user.email}',
                                 style: TextStyle(
                                   color: Colors.black,
+                                  fontSize: 18,
                                   fontWeight: FontWeight.w600,
-                                  fontSize: 20,
                                 ),
-                                overflow: TextOverflow.ellipsis,
+                                overflow: TextOverflow
+                                    .ellipsis, // Handles overflow by ellipsis
                               ),
                             ),
                           ),
@@ -112,40 +277,6 @@ class _UserProfilePageState extends State<UserProfilePage> {
                     width: double.infinity,
                     height: 100,
                     decoration: BoxDecoration(
-                      color: Colors.yellowAccent[700],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Phone Number: ${_userDoc['phoneNumber']}',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          SizedBox(height: 2),
-                          Text(
-                            'Email: ${_user.email}',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Container(
-                    width: double.infinity,
-                    height: 100,
-                    decoration: BoxDecoration(
                       color: Colors.greenAccent.shade400,
                       borderRadius: BorderRadius.circular(10),
                     ),
@@ -155,7 +286,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
                           Text(
-                            'Remaining Amount: ${_userDoc['remainingAmount']}',
+                            'Remaining Amount: ${userDoc['remainingAmount']}',
                             style: TextStyle(
                               color: Colors.black,
                               fontSize: 18,
@@ -164,7 +295,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
                           ),
                           SizedBox(height: 2),
                           Text(
-                            'Total Income: ${_userDoc['totalIncome']}',
+                            'Total Income: ${userDoc['totalIncome']}',
                             style: TextStyle(
                               color: Colors.black,
                               fontSize: 18,
@@ -173,7 +304,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
                           ),
                           SizedBox(height: 2),
                           Text(
-                            'Total Expense: ${_userDoc['totalExpense']}',
+                            'Total Expense: ${userDoc['totalExpense']}',
                             style: TextStyle(
                               color: Colors.black,
                               fontSize: 18,
@@ -197,22 +328,74 @@ class _UserProfilePageState extends State<UserProfilePage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text(
-                            'Last Updated: $formattedDate',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
+                          if (formattedDate.isNotEmpty)
+                            Text(
+                              'Last Updated: $formattedDate',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                          ),
                         ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        _dialogLogout(context);
+                      },
+                      child: Container(
+                        width: 150,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent.shade700,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  IconButton(
+                                    onPressed: () async {
+                                      _dialogLogout(context);
+                                    },
+                                    icon: isLogoutLoading
+                                        ? CircularProgressIndicator()
+                                        : Icon(
+                                            FontAwesomeIcons.rightFromBracket,
+                                            color: Colors.white,
+                                            size: 30,
+                                          ),
+                                  ),
+                                  Text(
+                                    'Logout',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ],
               ),
-            )
-          : Center(child: CircularProgressIndicator()),
+            );
+          }
+        },
+      ),
     );
   }
 }
