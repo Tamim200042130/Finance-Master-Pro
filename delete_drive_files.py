a54190c4d7237ffcda225c51eb78e94d35e86ccb
Md.Siamul Mubin Tamim <95132931+Tamim200042130@users.noreply.github.com>
2024-07-09 13:54:55 +0600

    slack-added

--- delete_drive_files.py
+++ delete_drive_files.py
@@ -4,15 +4,12 @@ import os
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
-# Decode the base64-encoded credentials
 credentials_base64 = os.getenv('GOOGLE_CREDENTIALS_BASE64')
 credentials_json = base64.b64decode(credentials_base64).decode('utf-8')
 
-# Save the credentials to a temporary file
 with open('credentials.json', 'w') as f:
     f.write(credentials_json)
 
-# Load credentials
 credentials = service_account.Credentials.from_service_account_file(
     'credentials.json',
     scopes=['https://www.googleapis.com/auth/drive']
@@ -22,7 +19,6 @@ drive_service = build('drive', 'v3', credentials=credentials)
 
 folder_id = os.getenv('FOLDER_ID')
 
-# List all files in the folder
 query = f"'{folder_id}' in parents"
 results = drive_service.files().list(q=query, fields="files(id, name)").execute()
 items = results.get('files', [])
