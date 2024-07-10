a7daad83d6281618408b016fcf47525d00efd956
Md.Siamul Mubin Tamim <95132931+Tamim200042130@users.noreply.github.com>
2024-07-10 19:38:32 +0600

    telegram-automation-3

--- send_telegram_message.py
+++ send_telegram_message.py
@@ -5,17 +5,35 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 TELEGRAM_THREAD_ID = os.getenv('TELEGRAM_THREAD_ID')
 MESSAGE = os.getenv('MESSAGE')
+APK_FILE_PATH = os.getenv('APK_FILE_PATH')
 
-url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+# Upload APK file to Telegram
+url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendDocument"
+files = {'document': open(APK_FILE_PATH, 'rb')}
 data = {
     "chat_id": TELEGRAM_CHAT_ID,
     "message_thread_id": TELEGRAM_THREAD_ID,
-    "text": MESSAGE,
 }
 
-response = requests.post(url, data=data)
+response = requests.post(url, data=data, files=files)
 
 if response.status_code == 200:
-    print("Message sent successfully")
+    document_link = response.json()['result']['document']['file_id']
+    print("APK file uploaded successfully")
+
+    # Send message with APK file link
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "message_thread_id": TELEGRAM_THREAD_ID,
+        "text": f"{MESSAGE}\nDownload Now: {document_link}",
+    }
+
+    response = requests.post(url, data=data)
+
+    if response.status_code == 200:
+        print("Message sent successfully")
+    else:
+        print(f"Failed to send message: {response.status_code} - {response.text}")
 else:
-    print(f"Failed to send message: {response.status_code} - {response.text}")
+    print(f"Failed to upload APK file: {response.status_code} - {response.text}")
