babc28991dbfa08d06d601e0cb6cf6b04f133df1
Md.Siamul Mubin Tamim <95132931+Tamim200042130@users.noreply.github.com>
2024-07-12 10:33:12 +0600

    Update update_pubspec_version.py

--- update_pubspec_version.py
+++ update_pubspec_version.py
@@ -4,13 +4,17 @@ import re
 with open('version.txt', 'r') as version_file:
     version = version_file.read().strip()
 
+# Ensure the version has three components
+if len(version.split('.')) == 2:
+    version = f"{version}.0"
+
 # Read the contents of pubspec.yaml
 with open('pubspec.yaml', 'r') as pubspec_file:
     pubspec_contents = pubspec_file.read()
 
 # Replace the version line with the new version
 new_pubspec_contents = re.sub(r'^version: .*$',
-                              f'version: {version}',
+                              f'version: {version}+1',
                               pubspec_contents,
                               flags=re.MULTILINE)
 
